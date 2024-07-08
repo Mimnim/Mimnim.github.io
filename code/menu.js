66bf84070fe38c750b9ddba88f275af8e80ffbcc
mrs12 <mrs12@aber.ac.uk>
2024-07-08 14:32:03 +0200

    added js for menu

---
+++ code/menu.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", loadMenu);
+
+function loadMenu(){
+    document.getElementById("menu").innerHTML = "<h1 class = 'heading left'><a href='home.html'>Home</a></h1> <h1 class = 'heading left'><a href='skills.html'>Skills</a></h1> <h1 class = 'heading left'>Projects</h1>";
+}
+
