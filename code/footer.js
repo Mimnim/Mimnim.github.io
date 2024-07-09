39a04bd67741dd7461faf3227fe640c62d7e8419
mrs12 <mrs12@aber.ac.uk>
2024-07-09 17:49:14 +0200

    added draft for batch fix

---
+++ code/footer.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", loadfooter);
+
+function loadfooter(){
+    document.getElementById("footer").innerHTML="<p>&#169; 2024 All Rights Reserved - Coded and Designed by Mim Small</p>"
+}
