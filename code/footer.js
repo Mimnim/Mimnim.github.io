09f7018ac5870eadd61e9ec22f0a26763a2696c9
mrs12 <mrs12@aber.ac.uk>
2024-07-22 21:23:37 +0100

    dleted extra files for pull

--- code/footer.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", loadfooter);
-
-function loadfooter(){
-    document.getElementById("footer").innerHTML="<p>&#169; 2024 All Rights Reserved - Coded and Designed by Mim Small</p>"
-}
