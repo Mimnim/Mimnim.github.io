8309b17f2795845e78ca192a22274db29c6c2cb8
Mimnim <mimnimdel@gmail.com>
2024-07-23 14:12:38 +0100

    added console debug log

--- code/javascript/iframe-resizer.js
+++ code/javascript/iframe-resizer.js
@@ -3,6 +3,7 @@ window.addEventListener('message', function (event) {
         console.log('Received height:', event.data.height, 'for iframe:', event.data.id);
         var iframe = document.getElementById(event.data.id);
         var total_height = pareseInt(event.data.height) + 10;
+        console.log('full height:', total_height);
         if (iframe) {
             iframe.style.height = total_height + 'px';
         }
