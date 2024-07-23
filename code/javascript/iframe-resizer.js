a4dc57c0fb494fee86d5abe7c698e1e89f0d196b
Mimnim <mimnimdel@gmail.com>
2024-07-23 14:10:57 +0100

    added 10 to hieght

--- code/javascript/iframe-resizer.js
+++ code/javascript/iframe-resizer.js
@@ -2,8 +2,9 @@ window.addEventListener('message', function (event) {
     if (event.data.height && event.data.id) {
         console.log('Received height:', event.data.height, 'for iframe:', event.data.id);
         var iframe = document.getElementById(event.data.id);
+        var total_height = pareseInt(event.data.height) + 10;
         if (iframe) {
-            iframe.style.height = event.data.height + 'px';
+            iframe.style.height = total_height + 'px';
         }
     }
 });
