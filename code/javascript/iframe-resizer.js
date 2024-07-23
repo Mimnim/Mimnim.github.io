6563d9b4d63de3860abfd9e88dc966544d3f7d92
Mimnim <mimnimdel@gmail.com>
2024-07-23 16:16:58 +0100

    removed margin and +10 in iframe resizer

--- code/javascript/iframe-resizer.js
+++ code/javascript/iframe-resizer.js
@@ -3,7 +3,7 @@ window.addEventListener('message', function (event) {
     if (event.data.height && event.data.id) {
         console.log('Received height:', event.data.height, 'for iframe:', event.data.id);
         var iframe = document.getElementById(event.data.id);
-        var total_height = pareseInt(event.data.height) + 10;
+        var total_height = pareseInt(event.data.height);
         console.log('full height:', total_height);
         if (iframe) {
             iframe.style.height = total_height + 'px';
