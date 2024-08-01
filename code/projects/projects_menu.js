9ef6e9021543b12948dd67045aa922a719064582
Mimnim Petite <mimnimdel@gmail.com>
2024-08-01 23:05:11 +0100

    fixed menu inside of individual project pages

---
+++ code/projects/projects_menu.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", loadMenu);
+
+function loadMenu(){
+    document.getElementById("menu").innerHTML = "<h1 class = 'heading left'><a href='../../index.html'>Home</a></h1> <h1 class = 'heading left'><a href='../../projects.html'>Projects</a></h1>";
+}
+
