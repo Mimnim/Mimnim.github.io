135848325e2a5599f3ca999e1d3c198d1bcb8a7f
Mimnim Petite <mimnimdel@gmail.com>
2024-11-21 16:52:11 -0700

    button test

--- code/javascript/menu.js
+++ code/javascript/menu.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", loadMenu);
 
 function loadMenu(){
-    document.getElementById("menu").innerHTML = "<h1 class = 'heading right'><a href='index.html'>Home</a></h1> <h1 class = 'heading right'><a href='projects.html'>Projects</a></h1><button type='button'>Click Me!</button>";
+    document.getElementById("menu").innerHTML = "<h1 class = 'heading right'><a href='index.html'>Home</a></h1> <h1 class = 'heading right'><a href='projects.html'>Projects</a></h1><button type='button' onclick='window.location.href='https://w3docs.com';'>Click Me!</button>";
 }
 
