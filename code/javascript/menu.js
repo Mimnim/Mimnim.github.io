43cf04e229aa7749320725ea63d84491494dd918
Mimnim Petite <mimnimdel@gmail.com>
2024-11-21 17:01:39 -0700

    fixed navbar

--- code/javascript/menu.js
+++ code/javascript/menu.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", loadMenu);
 
+let menu_text = "<h1 class = 'heading right'><a href='index.html'>Home</a></h1>" +
+                "<h1 class = 'heading right'><a href='projects.html'>Projects</a></h1>" +
+                "<h1 class = 'heading right'><a class='link-button' href='https://www.upwork.com/freelancers/~011c5bf8a4be4c9149?mp_source=share'>Upwork</a></h1>";
+
 function loadMenu(){
-    document.getElementById("menu").innerHTML = "<h1 class = 'heading right'><a href='index.html'>Home</a></h1> <h1 class = 'heading right'><a href='projects.html'>Projects</a></h1><button type='button' onclick='window.location.href='https://w3docs.com';'>Click Me!</button>";
+    document.getElementById("menu").innerHTML = menu_text;
 }
 
