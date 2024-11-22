4986b875f481d8ed4d5310274dcb2adf9b0ef6e9
Mimnim Petite <mimnimdel@gmail.com>
2024-11-21 17:13:48 -0700

    menu test

--- code/javascript/menu.js
+++ code/javascript/menu.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", loadMenu);
 
-let menu_text = "<h1 class = 'heading right'><a href='index.html'>Home</a></h1>" +
-                "<h1 class = 'heading right'><a href='projects.html'>Projects</a></h1>" +
-                "<h1 class = 'heading right'><a class='link_button' href='https://www.upwork.com/freelancers/~011c5bf8a4be4c9149?mp_source=share'>Upwork</a></h1>";
+let menu_text = "<h1 class=\"heading left\" id=\"signature\"> Mim's Portfolio</h1>"
+                "<h2 class = 'heading right'><a href='index.html'>Home</a></h2>" +
+                "<h2 class = 'heading right'><a href='projects.html'>Projects</a></h2>" +
+                "<h2 class = 'heading right'><a class='link_button' href='https://www.upwork.com/freelancers/~011c5bf8a4be4c9149?mp_source=share'>Upwork</a></h2>";
 
 function loadMenu(){
     document.getElementById("menu").innerHTML = menu_text;
