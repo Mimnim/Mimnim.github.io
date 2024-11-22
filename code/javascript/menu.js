9251ea9eb0732987635ab99c848b0870638eb15c
Mimnim Petite <mimnimdel@gmail.com>
2024-11-21 17:17:42 -0700

    simplified menu

--- code/javascript/menu.js
+++ code/javascript/menu.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", loadMenu);
 
-let menu_text = "<h1 class=\"heading left\" id=\"signature\"> Mim's Portfolio</h1>" +
-                "<h2 class = 'heading right'><a href='index.html'>Home</a></h2>" +
+let menu_text = "<h1 class='heading left'><a href='index.html'>Mim's Portfolio</a></h1>" +
                 "<h2 class = 'heading right'><a href='projects.html'>Projects</a></h2>" +
                 "<h2 class = 'heading right'><a class='link_button' href='https://www.upwork.com/freelancers/~011c5bf8a4be4c9149?mp_source=share'>Upwork</a></h2>";
 
