ba2baf8b7c1704359755ef79023245e87e180448
Mimnim Petite <mimnimdel@gmail.com>
2024-11-21 17:03:44 -0700

    fixed button

--- code/javascript/menu.js
+++ code/javascript/menu.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", loadMenu);
 
 let menu_text = "<h1 class = 'heading right'><a href='index.html'>Home</a></h1>" +
                 "<h1 class = 'heading right'><a href='projects.html'>Projects</a></h1>" +
-                "<h1 class = 'heading right'><a class='link-button' href='https://www.upwork.com/freelancers/~011c5bf8a4be4c9149?mp_source=share'>Upwork</a></h1>";
+                "<h1 class = 'heading right'><a class='link_button' href='https://www.upwork.com/freelancers/~011c5bf8a4be4c9149?mp_source=share'>Upwork</a></h1>";
 
 function loadMenu(){
     document.getElementById("menu").innerHTML = menu_text;
