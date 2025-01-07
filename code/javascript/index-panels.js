ae2ad068b6f93f5a39f4369c1f12bfede88988b2
Mimnim Petite <mimnimdel@gmail.com>
2025-01-07 20:47:19 +0000

    testing new way to make content panels

---
+++ code/javascript/index-panels.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", loadContent);
+
+let panels_dump = "";
+
+function loadContent() {
+  let promises = [];
+  
+  for (let i = 0; i < 8; i++) {
+    let doc = "part" + i.toString();
+    let promise = fetch("./index_panels/" + doc)
+      .then(response => response.text())
+      .then(data => data)
+      .catch(error => {
+        console.error('Error fetching content:', error);
+        return ""; 
+      });
+
+    promises.push(promise);
+  }
+
+  Promise.all(promises).then(results => {
+    panels_dump = results.join(""); // Join all results into a single string
+    document.getElementById('content-body').innerHTML = panels_dump;
+  });
+}
