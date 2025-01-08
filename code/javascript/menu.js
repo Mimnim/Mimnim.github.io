15118f06f4a7de36e11c71630fd5d552c983e78a
Mimnim Petite <mimnimdel@gmail.com>
2025-01-08 21:36:41 +0000

    changed menu heading tage

--- code/javascript/menu.js
+++ code/javascript/menu.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", loadMenu);
 
 let menu_text = "<h1 class='heading left'><a href='index.html'>Mim's Portfolio</a></h1>" +
                 "<div id ='menu_right'>" +
-                "<h2 class = 'heading right'><a href='projects.html'>Projects</a></h2>" +
-                "<h2 class = 'heading right'><a class='link_button' href='https://www.upwork.com/freelancers/~011c5bf8a4be4c9149?mp_source=share'>Upwork</a></h2>" +
+                "<h1 class = 'heading right'><a href='projects.html'>Projects</a></h2>" +
+                "<h1 class = 'heading right'><a class='link_button' href='https://www.upwork.com/freelancers/~011c5bf8a4be4c9149?mp_source=share'>Upwork</a></h2>" +
                 "</div>";
 
 function loadMenu(){
